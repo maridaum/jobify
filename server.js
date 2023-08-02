fcbe203e32827daa8a961dc2e5902aae5c35d6d3
Marida Um <maridaum7@yahoo.com>
2023-08-02 13:33:42 -0700

    Fix front-end.

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ cloudinary.config({
 
 //syntax for es6. different for commonjs
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -53,7 +54,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 //this middleware takes care of requests that do not match anythign in our server
